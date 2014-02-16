47dbe61b07f4a1f42ec95fe69220bfe6a20f0229
Rui LI <einst1993@gmail.com>
2014-02-17 03:14:52 +0800

    add char/string => scancode/vk_code map

---
+++ scancode_map.py
@@ -0,0 +1,8 @@
+
+key_code_map = {
+	'a' : 0,
+	'A' : 0
+}
+
+def scancode(key):
+	return key_code_map[key];
---
+++ virtual_key_map.py
@@ -0,0 +1,8 @@
+
+virtual_key_map = {
+	'a' : 0,
+	'A' : 0
+}
+
+def virtual_key(key):
+	return virtual_key_map[key];
