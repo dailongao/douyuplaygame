112658e3ff277550145f33ad4d49fc21536d5e54
Rui LI <einst1993@gmail.com>
2014-02-17 22:23:45 +0800

    chat is available

--- chat.py
+++ chat.py
@@ -1,36 +1,102 @@
-import socket 
+﻿import socket 
 import thread
+import threading
 import time
+import sys
 
+# 
 HOST='220.194.199.222'
 PORT=27001
 BUFSIZ=2048
 ADDR=(HOST,PORT)
+DEFAULT_ROOM=3258
 
-login="\x66\x00\x00\x00\x66\x00\x00\x00\xb1\x02\x00\x00type@=loginreq/username@=acfun_k66hppfz/password@=ee96f962b58e1574007a3af2d07195df/roomid@=8/\x00"
-joingroup="\x27\x00\x00\x00\x27\x00\x00\x00\xb1\x02\x00\x00type@=joingroup/rid@=8/gid@=0/\x00"
-keeplive="\x21\x00\x00\x00\x21\x00\x00\x00\xb2\x02\x00\x00type@=keeplive/tick@=41/\x00"
+#
+login_header = "\x59\x00\x00\x00\x59\x00\x00\x00\xb1\x02\x00\x00"
+joingroup_header = "\x2a\x00\x00\x00\x2a\x00\x00\x00\xb1\x02\x00\x00"
+keeplive_header = "\x21\x00\x00\x00\x21\x00\x00\x00\xb2\x02\x00\x00"
 
+login_req = "type@=loginreq/username@=acfun_k66hppfz/password@=1234567890123456/roomid@=%d/\x00"
+joingroup_req = "type@=joingroup/rid@=%d/gid@=0/\x00"
+keeplive_req = "type@=keeplive/tick@=41/\x00"
+
+login = login_header + login_req
+joingroup = joingroup_header + joingroup_req
+keeplive = keeplive_header + keeplive_req
+
+# -,-
+TERMINATE = False
+
+#
+def msg(str):
+	return str[12:]
+
+#
+def recv_thread_func(hfunc,a2):
+	while True:
+		if TERMINATE:
+			return
+		time.sleep(0.1)
+		data = tcpCliSock.recv(BUFSIZ)
+		str = msg(data)
+		#print(str)
+		hfunc(str)
+
+#		
+def keeplive_thread_func(a1,a2):
+	while True:
+		if TERMINATE:
+			return
+		time.sleep(5)
+		tcpCliSock.send(keeplive)
+		
 # 
-tcpCliSock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-tcpCliSock.connect(ADDR)
+def open_socket():
+	global tcpCliSock
+	tcpCliSock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	tcpCliSock.connect(ADDR)
+	
+#	
+def close_socket():
+	tcpCliSock.close()
 
-# login
-tcpCliSock.send(login)
-data=tcpCliSock.recv(BUFSIZ)
-print(data)
+#
+def dy_login(roomid):
+	print 'douyu.tv login'
+	tcpCliSock.send(login % roomid)
 
-# join group
-tcpCliSock.send(joingroup)
-data=tcpCliSock.recv(BUFSIZ)
-print(data)
+#
+def dy_join_group(roomid):
+	print 'douyu.tv join group'
+	tcpCliSock.send(joingroup % roomid)
 
-# loop : keep live & get data
-while True:
-	time.sleep(1)
-	tcpCliSock.send(keeplive)
+#	
+def gao(room_id, hfunc):
+	print 'Kai Gao , Room ID :',room_id
 
-	data=tcpCliSock.recv(BUFSIZ)
-	print(data)
+	rt = threading.Thread(target=recv_thread_func, args=(hfunc,0))
+	kt = threading.Thread(target=keeplive_thread_func, args=(0,0))
+	
+	open_socket()
+	
+	dy_login(room_id)
+	dy_join_group(room_id)
+	
+	rt.start()
+	#rt.join()
+	
+	raw_input("press any key to stop")
+	close_socket()
+	TERMINATE = True
+	
+	#kt.start()
+	#kt.join()
+	#while True:
+	#	time.sleep(1)
 
-tcpCliSock.close()
+def print_message(str):
+	print str
+
+if __name__ == '__main__':
+	gao(DEFAULT_ROOM, print_message)
+
